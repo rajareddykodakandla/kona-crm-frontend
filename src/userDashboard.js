39f814038760cabd1e45b481b67ba9118b88648a
raja <rajareddykodakandla>
2021-03-26 22:48:01 +0530

    Implement Dashboard page for logged in user(CRR-6)

--- src/userDashboard.js
+++ src/userDashboard.js
@@ -1,12 +1,31 @@
 import React from 'react'
-
-class userDashboard extends React.Component{
-    render(){
-        console.log(this.props)
-        return(
+import { NavLink } from 'react-router-dom'
+import './userDashboard.css'
+class userDashboard extends React.Component {
+    render() {
+        if(!sessionStorage.getItem("token")){
+            this.props.history.push("/login")
+        }
+        return (
             <div>
-              <h1>This is userDashboard</h1>
+                <div id="mySidenav" className="sidenav">
+                    <h3 className="header" > <span style={{ color: "#FFFFFF" }}>Kona</span><span style={{ color: "#E9204F" }}>digital.ai</span></h3>
+
+                    <div className="links">
+                        <NavLink to="/dashboard" >Dashboard</NavLink>
+                        <NavLink to="/dashboard">Leads</NavLink>
+                        <NavLink to="/dashboard">Report</NavLink>
+                        <NavLink to="/dashboard">Settings</NavLink>
+
+                        <NavLink to="/changepassword" >change password</NavLink>
+                    </div>
+
+                </div>
+                <div className="dashboard" >
+                    <h1>Hello welcome to user Dashboard</h1>
+                </div>
             </div>
+
         )
     }
 }
